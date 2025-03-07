39e5d8a80ef3b34fb3a08f3dd068526d70bebf5a
kritz999 <kritikabdas007@gmail.com>
2025-03-07 12:03:29 +0530

    [kritika] add: Display logo of BridgeLabz

---
+++ DisplayLogo/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title: string = 'BridgeLabz';
+  logoUrl: string = 'https://media.licdn.com/dms/image/v2/C510BAQEYyeT3P0H_mw/company-logo_200_200/company-logo_200_200/0/1630611149363/bridgelabz_com_logo?e=2147483647&v=beta&t=TFgRiaA55f57NqQFnUbnsKu3mQo7c-LOfD8_iNSgtNM';
+}
