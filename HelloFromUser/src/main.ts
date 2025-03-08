3aa2645e03e37ebd5453cd8eafa3fd96f192e951
kritz999 <kritikabdas007@gmail.com>
2025-03-08 15:00:51 +0530

    [kritika] add: Hello from user

---
+++ HelloFromUser/src/main.ts
@@ -0,0 +1,11 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { FormsModule } from '@angular/forms';
+import { importProvidersFrom } from '@angular/core';
+bootstrapApplication(AppComponent,
+  {
+    providers: [importProvidersFrom(FormsModule)]
+  }
+)
+  .catch((err) => console.error(err));
